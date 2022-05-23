6e732067ed39c85bf331c0fd8287cd99e96727ba
Daniel Ortega <83851986+rekirem@users.noreply.github.com>
2022-05-23 18:31:51 -0400

    Create mutation.js

---
+++ mutation.js
@@ -0,0 +1,8 @@
+function mutation(arr) {//Input must be an array with 2 strings
+    let str1 = arr[0].toLowerCase();
+    let str2 = arr[1].toLowerCase();
+    for (let i = 0; i < str2.length; i++) {
+        if (str1.indexOf(str2[i]) < 0) return false;
+    }
+    return true;
+}
